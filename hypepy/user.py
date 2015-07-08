a60dc70bbf4e309747c3b235be75d20bb455cc01
Kevin Wu <me@kevinformatics.com>
2015-07-08 00:05:58 -0700

    Get the join date, favorites and friends count for users

--- hypepy/user.py
+++ hypepy/user.py
@@ -1,16 +1,25 @@
+import datetime
+import locale
 from .section import Section
-
 from .hypem_urls import *
 from hypepy import session
 from bs4 import BeautifulSoup
 
 class User(object):
 
-    def __init__(self, username):
+    def __init__(self, username, join_date=None, favorites_count=None,
+                 friends_count=None):
+
         self.username = username
 
         self.url = HYPEM_USER_URL.format(username)
         self._friends = None
+
+        self._join_date = join_date
+        self._favorites_count = favorites_count
+        self._friends_count = friends_count
+
+        locale.setlocale( locale.LC_ALL, 'en_US.UTF-8' )
 
     def feed(self):
         return Section("{}/feed".format(self.username), page_limit=None)
@@ -44,8 +53,53 @@ class User(object):
 
             for user in users:
                 username = user.find('a')['href'].lstrip('/')
-                self._friends.append(User(username))
+
+                join_date, favorites_count, friends_count = \
+                        self._get_join_date_favorites_friends_count_from_soup(user)
+
+                self._friends.append(User(username, join_date,
+                                          favorites_count, friends_count))
 
             page_num += 1
 
         return self._friends
+
+    def join_date(self):
+        self._get_join_date_favorites_friends_count()
+        return self._join_date
+
+    def favorites_count(self):
+        self._get_join_date_favorites_friends_count()
+        return self._favorites_count
+
+    def friends_count(self):
+        self._get_join_date_favorites_friends_count()
+        return self._friends_count
+
+    def _get_join_date_favorites_friends_count(self):
+        if self._join_date is None or self._favorites_count is None or \
+           self._friends_count is None:
+
+            req = session.get(self.url)
+            soup = BeautifulSoup(req.text)
+            self._join_date, self._favorites_count, self._friends_count = \
+                    self._get_join_date_favorites_friends_count_from_soup(soup)
+
+    @staticmethod
+    def _get_join_date_favorites_friends_count_from_soup(soup):
+            # find the join date, parse out the month, day (strip endings),
+            # and year and parse the string into a python date
+            join_date = soup.find('p', class_='join-date').get_text(strip=True)
+            _, month, day, year = join_date.split()
+            day = day.replace('st', '').replace('nd', '').replace('rd', '').replace('th', '')
+            day = int(day.rstrip(','))
+
+            join_time = datetime.datetime.strptime("{:02d} {} {}".format(day, month, year), '%d %b %Y')
+            join_date = join_time.date()
+
+            favorites_count, friends_count = soup.find_all(class_='big-num')
+
+            favorites_count = locale.atoi(favorites_count.get_text(strip=True))
+            friends_count = locale.atoi(friends_count.get_text(strip=True))
+
+            return join_date, favorites_count, friends_count
